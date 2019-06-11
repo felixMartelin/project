1cf24cca9966f255281eec4022ed9c00cb8e0623
Felix <felix.martelin@etu.univ-lyon1.fr>
2019-06-11 13:30:16 +0200

    modelDAOBrawler

---
+++ app/Modeles/BrawlerDAO.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Modeles;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class BrawlerDAO extends Model
+{
+    //
+    public function getLesBrawlers()
+    {
+        $Brawlers= DB::table('Brawler')->get();
+        $LesBrawlers=array();
+
+        foreach($Brawlers as $brawler)
+        {
+            $idBrawler=$brawler->IdBrawler;
+            $LesBrawlers[$idBrawler]=$this->creerBrawler($brawler);
+        }
+
+        return $LesBrawlers;
+    }
+
+    public function creerBrawler(\stdClass $unBrawler)
+    {
+        $leBrawler = new Brawler();
+        $leBrawler->setIdBrawler($unBrawler->IdBrawler);
+        $leBrawler->setNomBrawler($unBrawler->NomBrawler);
+        $leBrawler->setRareteBrawler($unBrawler->RareteBrawler);
+        $leBrawler->setVieBrawler($unBrawler->VieBrawler);
+        $leBrawler->setDegatBrawler($unBrawler->DegatBrawler);
+        $leBrawler->setDegatSuperBrawler($unBrawler->DegatSuperBrawler);
+        $leBrawler->setStarPowerBrawler($unBrawler->StarPowerBrawler);
+        $leBrawler->setDescStarPower($unBrawler->DescStarPower);
+
+        return $leBrawler;
+    }
+
+    public function getUnBrawler($id)
+    {
+        $Brawlers= DB::table('Brawler')->get();
+
+        foreach($Brawlers as $brawler)
+        {
+            $idBrawler=$brawler->IdBrawler;
+            if($idBrawler==$id)
+            {
+                return $this->creerBrawler($brawler);
+            }
+        }
+    }
+}
