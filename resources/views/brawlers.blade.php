bc259696b384318d46efdc1d708209f22c9d3542
Felix <felix.martelin@etu.univ-lyon1.fr>
2019-06-11 13:45:08 +0200

    Controller + VueBrawler

--- resources/views/brawlers.blade.php
+++ resources/views/brawlers.blade.php
@@ -7,3 +7,20 @@
 @section('titreItem')
 
     @endsection
+
+@section('contenu')
+    @foreach($lebrawler as $brawler)
+        <h2> Fiche Descriptive du Brawler :</h2>
+        <br/>
+        <h4> Nom du Brawler : {{ $brawler->getNomBrawler() }} </h4>
+        <h4> Rareté du Brawler : {{ $brawler->getRareteBrawler() }} </h4>
+        <h4> Description du Brawler : {{ $brawler->getNomBrawler() }} </h4>
+        <h4> Nom du Stat Power : {{ $brawler->getStarPowerBrawler() }} </h4>
+        <h4> Description du Star Power : {{ $brawler->getDescStarPower() }} </h4>
+        <br/><br/>
+        <h2> Statistiques du Brawler : </h2>
+        <br/>
+        <h4> Point de Vie du Brawler : {{ $brawler->getVieBrawler() }} </h4>
+        <h4> Dégâts du Brawler : {{ $brawler->getDegatBrawler() }} </h4>
+        <h4> Dégâts du Super : {{ $brawler->getDegatSuperBrawler() }} </h4>
+    @endsection
